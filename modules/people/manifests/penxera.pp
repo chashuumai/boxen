c4c6b34b764e1e2b505104530d36e3f135bd8790
penxera <kool.arakaze@gmail.com>
2014-02-03 19:43:14 +0900

    個人設定

--- modules/people/manifests/penxera.pp
+++ modules/people/manifests/penxera.pp
@@ -3,48 +3,72 @@ class people::penxera {
   #include dropbox
   #include sublimetext
   include osx
+  # lib
+  include zsh
 
-    'Kobito':
-        source   => "http://kobito.qiita.com/download/Kobito_v1.2.0.zip",
-        provider => compressed_app;
+  # local application for develop
+  include iterm2::stable
+  include sublime_text_2
+  sublime_text_2::package { 'Emmet':
+    source => 'sergeche/emmet-sublime'
+  }
+  include firefox
+  include chrome
+  include cyberduck
+
+  # local application for utility
+  include dropbox
+
+
 
   # homebrewでインストール
   package {
-    'zsh':
-        install_options => [
-          '--disable-etcdir'
-        ]
     [
-      'tmux',
-      'reattach-to-user-namespace',
+      'readline', # use for rails
+      'tree',
+      'git-extras',
+      'z',
       'tig',
     ]:
   }
 
-  file_line { 'add zsh to /etc/shells':
-  path    => '/etc/shells',
-  line    => "${boxen::config::homebrewdir}/bin/zsh",
-  require => Package['zsh'],
-  before  => Osx_chsh[$::luser];
-}
-osx_chsh { $::luser:
-  shell   => "${boxen::config::homebrewdir}/bin/zsh";
-}
+  # local application
+  package {
+    # utility
+    # 'XtraFinder':
+    # source   => "http://www.trankynam.com/xtrafinder/downloads/XtraFinder.dmg",
+    # provider => pkgdmg;
+  }
 
+
+  #file_line { 'add zsh to /etc/shells':
+  #path    => '/etc/shells',
+  #line    => "${boxen::config::homebrewdir}/bin/zsh",
+  #require => Package['zsh'],
+  #before  => Osx_chsh[$::boxen_user];
+  #}
+
+  #osx_chsh { $::boxen_user:
+  #  shell   => "${boxen::config::homebrewdir}/bin/zsh";
+  #}
+
+
+  # dotfile setting
   $home     = "/Users/${::boxen_user}"
-  $src      = "${home}/src"
-  $dotfiles = "${src}/dotfiles"
+  #$src     = "${home}/src"
+  $dotfiles = "${home}/dotfiles"
 
-  # ~/src/dotfilesにGitHub上のpenxera/dotfilesリポジトリを
-  # git-cloneする。そのとき~/srcディレクトリがなければいけない。
+  # ~/dotfilesにGitHubのpenxera/dotfiles git-clone
   repository { $dotfiles:
     source  => "penxera/dotfiles",
-    require => File[$src]
+    # require => File[$src]
   }
-  # git-cloneしたらインストールする
-  exec { "sh ${dotfiles}/install.sh":
+  # git-cloneしたらインストール
+  exec { "submodule-clone":
+    # "sh ${dotfiles}/install.sh":
     cwd => $dotfiles,
-    creates => "${home}/.zshrc",
+    # creates => "${home}/.zshrc",
+    command => 'git submodule init && git submodule update'
     require => Repository[$dotfiles],
   }
 }
